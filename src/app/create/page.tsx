514d18d1eeb4ed33b8771258a2a1a700b01a350c
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-07 21:28:54 +0700

    AI-8 | feat: wrap FormLogoMain component in Suspense for improved loading handling | Deploy

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,12 @@
 import { FormLogoMain } from "@/components/forms/form-logo-main";
+import { Suspense } from "react";
 
 export default function CreatePage() {
   return (
     <main className="w-full max-w-7xl mx-auto px-4 py-24">
-      <FormLogoMain />
+      <Suspense>
+        <FormLogoMain />
+      </Suspense>
     </main>
   );
 }
